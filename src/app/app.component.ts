732ac32c924cc30427198b67d23588460282a54a
Vivekanand P V <vivekanandpv@outlook.com>
2019-09-08 17:36:04 +0530

    Built-in pipes demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Learning value-binding';
+
+  //  Fields for built-in pipe demonstration
+  pi = 3.14159265359;
+  today = new Date();
+  amount = 12.5236;
+  rate = 0.87;
+  car = {
+    make: 'BMW',
+    model: '530d',
+    owner: {
+      name: 'Rajesh',
+      city: 'Bengaluru'
+    }
+  };
 }
