ba94965802d4f7a5278697893948a30a7b5748ef
Vivekanand P V <vivekanandpv@outlook.com>
2019-09-08 18:02:26 +0530

    ngOnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Learning value-binding';
+
+  constructor() {
+    console.log('Constructor called');
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called');
+  }
 }
